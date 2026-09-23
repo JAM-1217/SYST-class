f8acd0cbb9908666bf4fac0f1d04f212df28068e
John Mazer <mazerjo@sheridancollege.ca>
2026-09-23 09:48:03 -0400

    Revert "testing fetch"
    This reverts commit 0415bf6176328e2708aea2ca77953e613a2ca0b8.

--- src/hellopkg/StudentList.java
+++ src/hellopkg/StudentList.java
@@ -9,7 +9,6 @@ package hellopkg;
  * This comment has been added to show push to remote repo
  * @author John
  * This is a change for testing
- * Testing
  */
 public class StudentList {
     public static void main(String[] args){
