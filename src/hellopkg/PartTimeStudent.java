ebe3b2591454d45454ad2b975780c54a4bf4222e
John Mazer <mazerjo@sheridancollege.ca>
2026-09-23 09:59:06 -0400

    added new class called part time student

---
+++ src/hellopkg/PartTimeStudent.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hellopkg;
+
+/**
+ *
+ * @author John
+ */
+public class PartTimeStudent extends Student {
+    private int numOfCourse;
+    
+      public PartTimeStudent(int numOfCourse, String sID, String sName) {
+        super(sID, sName);
+        this.numOfCourse = numOfCourse;
+    }  
+
+    public int getNumOfCourse() {
+        return numOfCourse;
+    }
+
+    public void setNumOfCourse(int numOfCourse) {
+        this.numOfCourse = numOfCourse;
+    }
+
+
+    
+    
+}
